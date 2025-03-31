b42e69e89f5bf2f9309eec51e702b406a5ae0371
Paulo Rodrigues <paulo.jorge.rodrigues@gmail.com>
2025-03-31 01:53:06 +0100

    solution

--- src/makeCalculator.js
+++ src/makeCalculator.js
@@ -5,6 +5,43 @@
  */
 function makeCalculator() {
   // write code here
+  const calc = {
+    balance: 0,
+
+    operate: function (f, value) {
+      f(value);
+
+      return this;
+    },
+
+    add: function (value) {
+      calc.balance += value;
+    },
+
+    subtract: function (value) {
+      calc.balance -= value;
+    },
+
+    divide: function (value) {
+      calc.balance /= value;
+    },
+
+    multiply: function (value) {
+      calc.balance *= value;
+    },
+
+    get result() {
+      return calc.balance;
+    },
+
+    reset: function () {
+      calc.balance = 0;
+
+      return this;
+    },
+  };
+
+  return calc;
 }
 
 module.exports = makeCalculator;
